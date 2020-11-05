f3bd86f74ed2e8e6aa63369bb6c2bbc30b1914af
fleurblanckaert <60987478+fleurblanckaert@users.noreply.github.com>
2020-11-04 22:48:40 -0500

    Initial Commit

---
+++ src/ca/durhamcollege/Main.java
@@ -0,0 +1,11 @@
+//name: Fleur Blanckaert
+package ca.durhamcollege;
+
+import static java.lang.System.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        out.println("Hello, World!");
+    }
+}
